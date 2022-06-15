1d4696daaeec81fe9650bfc31f50522adf9e8c81
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-15 22:36:47 +0200

    definiti valori di sconto

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@
 const numberKm = document.getElementById('number-km').value;
 const userAge = document.getElementById('user-age').value;
 const submitButton = document.getElementById('submit-button');
-// let ticketPriceUnderage;
-// let ticketPriceOverage;
+const pricePerKm = 0.26;
+let ticketPrice = (numberKm * pricePerKm);
+let ticketPriceUnderage;
+let ticketPriceOverage;
 
 // // (Se) Prima condizione: minore di 18 anni
 // if (userAge < 18) {
@@ -18,13 +20,18 @@ const submitButton = document.getElementById('submit-button');
 // }
 
 submitButton.addEventListener('click', function () {
-    console.log('ciao');
 
     console.log(document.getElementById('number-km').value);
 
     console.log(document.getElementById('user-age').value);
 
-    const pricePerKm = 0.26;
-    let ticketPrice = (document.getElementById('number-km').value * pricePerKm) + ' €';
+    let ticketPrice = (document.getElementById('number-km').value * pricePerKm);
     console.log(ticketPrice);
+
+    let ticketPriceUnderage = ticketPrice - (ticketPrice * 15 / 100) + " €";
+    console.log(ticketPriceUnderage);
+
+    let ticketPriceOverage = ticketPrice - (ticketPrice * 35 / 100) + " €";
+    console.log(ticketPriceOverage);
+
 })
