f499f8a5ede4076e79ffa7ccf7a1e2ae2c34b05f
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-15 16:16:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// const numberKm = parseInt('Quanti kilometri vuoi percorrere?');
+// const userAge = parseInt('Quanti anni hai?');
+
+// const numberKm = document.getElementById('number-km');
+// const userAge = document.getElementById('user-age').innerHTML;
+const submitButton = document.getElementById('submit-button');
+// const pricePerKm = 0.26;
+// let ticketPrice = (numberKm * pricePerKm);
+// let ticketPriceUnderage;
+// let ticketPriceOverage;
+
+// // (Se) Prima condizione: minore di 18 anni
+// if (userAge < 18) {
+//     ticketPriceUnderage = ticketPrice - (ticketPrice * 15 / 100) + " €";
+// }
+
+// // (Altrimenti se) Seconda condizione: maggiore o uguale di 65 anni
+// else if (userAge >= 65) {
+//     ticketPriceOverage = ticketPrice - (ticketPrice * 35 / 100) + " €";
+// }
+
+submitButton.addEventListener('click', function () {
+    console.log('ciao')
+    // const numberKm = (document.getElementById('number-km').value);
+    // console.log('numberkm');
+})
